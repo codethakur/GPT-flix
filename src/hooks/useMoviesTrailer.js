39060deba7765532085c4a3d2a6e7ef8ce125b80
codethakur <kishankt2017@gmail.com>
2024-04-18 12:50:40 +0530

    useMovies hook updated

--- src/hooks/useMoviesTrailer.js
+++ src/hooks/useMoviesTrailer.js
@@ -13,7 +13,7 @@ const useMoviesTrailer = (movieId) => {
       API_OPTIONS
     );
     const json = await data.json();
-    const filteredData = json.results.filter((video) => video.type === "Trailer");//type: "Trailer
+    const filteredData = json.results.filter((video) => video.type === "Trailer");
     const trailer = filteredData.length ? filteredData[0] : json.results[0];
     
     dispatch(addTrailerVideo(trailer));
